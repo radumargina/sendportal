27470fd1166a70cefa96b2f1b356493e22231c8b
Radu Margina <ip.radu@gmail.com>
2025-03-18 19:55:19 +0200

    Logos

--- resources/views/auth/partials/logo.blade.php
+++ resources/views/auth/partials/logo.blade.php
@@ -1,3 +1,4 @@
 <div class="logo text-center">
-    <img src="{{ asset('/vendor/sendportal/img/logo-gray.png') }}" alt="SendPortal" width="225px" class="my-5">
+    <img src="{{ asset('/images/lfx_sender_logo_dark.png') }}" alt=""  alt="LFXSender" width="255px" class="my-5">
+    {{-- <img src="{{ asset('/vendor/sendportal/img/logo-gray.png') }}" alt="SendPortal" width="225px" class="my-5"> --}}
 </div>
